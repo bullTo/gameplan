3b0c9ac5a6dfaa5d687e96e8903abc2ce6a72def
Igor <igorlazic525@gmail.com>
2025-07-09 21:42:13 -0400

    cors error

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -47,6 +47,18 @@ const isValidPassword = (password) => {
 };
 
 exports.handler = async (event) => {
+  // 🔧 Handle preflight request
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://gameplanai.io',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      },
+      body: '',
+    };
+  }
   // Only allow POST requests
   if (event.httpMethod !== 'POST') {
     return {
